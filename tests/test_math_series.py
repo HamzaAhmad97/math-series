3d8bebbb1e0b9b36bf9fb726030b3ff553d43bef
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-11 22:31:02 +0300

    added the first test for the fibonacci function

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,13 @@
 from math_series import __version__
+from math_series import fibonacci, lucas, sum_series
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fabonacci_with_a_number_0():
+    expected = 0
+    index = 0
+    actual = fibonacci(index)
+    assert actual == expected
